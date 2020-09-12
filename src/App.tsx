7a76058f73e704a37a0477f5be19ec3a5849d5a0
Anıl Şenay <anilsenay3@gmail.com>
2020-09-12 23:27:12 +0300

    Install Sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import style from "./App.module.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={style.App}>
+      <header className={style["App-header"]}>
+        <img src={logo} className={style["App-logo"]} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={style["App-link"]}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
