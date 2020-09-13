ede67f1e2e4b1b310789bbfb406ac4a294b558e6
Anıl Şenay <anilsenay3@gmail.com>
2020-09-13 22:01:11 +0300

    Add meal photos

---
+++ src/assets/images/meals/index.js
@@ -0,0 +1,7 @@
+import image0 from "./image-0.png";
+import image1 from "./image-1.png";
+import image2 from "./image-2.png";
+import image3 from "./image-3.png";
+import image4 from "./image-4.png";
+
+export const images = [image0, image3, image4, image1, image2];
