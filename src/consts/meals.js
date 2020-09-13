7a492db022abdbc503bcd2aa0057af71c9e63283
Anıl Şenay <anilsenay3@gmail.com>
2020-09-13 22:01:27 +0300

    Create photoShown component
    Former-commit-id: 2998c2aaf93c5378e23fc7f3c9aa84c2753c24a9

---
+++ src/consts/meals.js
@@ -0,0 +1,32 @@
+const meals = [
+  {
+    name: "",
+    price: 35,
+    information: "",
+    backgroundColor: "#EAFFE2",
+  },
+  {
+    name: "",
+    price: 35,
+    information: "",
+    backgroundColor: "#EAFFE2",
+  },
+  {
+    name: "",
+    price: 35,
+    information: "",
+    backgroundColor: "#EAFFE2",
+  },
+  {
+    name: "",
+    price: 35,
+    information: "",
+    backgroundColor: "#EAFFE2",
+  },
+  {
+    name: "",
+    price: 35,
+    information: "",
+    backgroundColor: "#EAFFE2",
+  },
+];
