9b071795764f72d06a76ca9767163f5230c27079
Anıl Şenay <anilsenay3@gmail.com>
2020-09-14 15:29:57 +0300

    Add fade animation for changing images
    Former-commit-id: eb86c4c1567bbe58395c74c1fca87aee89999f7b

--- src/components/Spin/photo_shown.tsx
+++ src/components/Spin/photo_shown.tsx
@@ -1,5 +1,4 @@
 import React, { ReactElement } from "react";
-
 import styles from "./style.module.scss";
 
 import { useStore } from "../../hooks/store.hook";
@@ -10,12 +9,20 @@ interface Props {}
 
 export default function PhotoShown({}: Props): ReactElement {
   const index = useStore((state) => state.index);
+
   return (
-    <img
-      src={images[index % 5]}
-      className={styles.photoShown}
-      alt="spin"
-      loading="lazy"
-    />
+    <>
+      {images.map((item, i) => {
+        return (
+          <img
+            src={images[i]}
+            className={styles.photoShown}
+            alt="spin"
+            loading="lazy"
+            style={{ opacity: ((index % 5) + 5) % 5 === i ? 1 : 0 }}
+          />
+        );
+      })}
+    </>
   );
 }
