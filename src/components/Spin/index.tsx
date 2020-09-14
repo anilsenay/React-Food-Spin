b6fc5c13aeb66618828ab594f55fb1721339d8d3
Anıl Şenay <anilsenay3@gmail.com>
2020-09-14 20:17:08 +0300

    Fix spin button props error
    Former-commit-id: cf3b67d54e22d15d3e319489b6a2b73a4d0e7068

--- src/components/Spin/index.tsx
+++ src/components/Spin/index.tsx
@@ -30,7 +30,7 @@ export default function Spin(): ReactElement {
         />
       </div>
       <PhotoShown />
-      <Button setImageIndex={setImageIndex} imageIndex={imageIndex} />
+      <Button />
     </>
   );
 }
