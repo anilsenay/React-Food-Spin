3587a1668d351775a6b94fc5ff4f2e5b88e7acb8
Anıl Şenay <anilsenay3@gmail.com>
2020-09-14 17:02:34 +0300

    Add rotate animation and fetch static data
    Former-commit-id: c3035fe527610f245eeaf337c6a5251deaab2aaf

--- src/components/Spin/index.tsx
+++ src/components/Spin/index.tsx
@@ -2,11 +2,12 @@ import React, { ReactElement, useState } from "react";
 import styles from "./style.module.scss";
 
 import spinImage from "../../assets/images/spin.svg";
+import meals from "../../consts/meals";
 
 import Button from "./spin_button";
+import PhotoShown from "./photo_shown";
 
 import { useStore } from "../../hooks/store.hook";
-import PhotoShown from "./photo_shown";
 
 interface Props {}
 
@@ -15,7 +16,12 @@ export default function Spin({}: Props): ReactElement {
   const index = useStore((state) => state.index);
   return (
     <>
-      <div className={styles.container}>
+      <div
+        className={styles.container}
+        style={{
+          backgroundColor: meals[((index % 5) + 5) % 5].backgroundColor,
+        }}
+      >
         <img
           src={spinImage}
           className={styles.spinImage}
--- src/consts/meals.js
+++ src/consts/meals.js
@@ -1,32 +1,44 @@
 const meals = [
   {
-    name: "",
-    price: 35,
-    information: "",
+    name: "Green Goddess Chicken Salad",
+    price: 32,
+    information:
+      "It is a non vegetarian salad which consists of the green goddess dressing mixed with chicken, peppers, olives and celery. ",
     backgroundColor: "#EAFFE2",
+    textColor: "#54BF29",
   },
   {
-    name: "",
-    price: 35,
-    information: "",
-    backgroundColor: "#EAFFE2",
+    name: "Agnolotti Pasta Salad with Ricotta",
+    price: 30,
+    information:
+      "Agnolotti Pasta Salad with Ricotta which consists of agnolotti or ravioli mixed with dill, flat-leaf parsley leaves, basil leaves and ricotta.",
+    backgroundColor: "#FFFAE1",
+    textColor: "#E8C518",
   },
   {
-    name: "",
-    price: 35,
-    information: "",
-    backgroundColor: "#EAFFE2",
+    name: "Watermelon Radish Salad with Sesame Lime Ginger Vinaigrette",
+    price: 38,
+    information:
+      "Beautiful curly tendrils of peppery arugula are tossed with bright, crisp slices of watermelon radish. Sweet Cara Cara orange supremes and the slight saltiness of crumbled feta are the perfect addition. Drizzle a little homemade Sesame Lime Ginger Vinaigrette and you have the most beautiful spring salad to celebrate upcoming Earth Day",
+    backgroundColor: "#FCDFE7",
+    textColor: "#EC819E",
   },
   {
-    name: "",
-    price: 35,
-    information: "",
-    backgroundColor: "#EAFFE2",
+    name: "Greek Salmon Salad",
+    price: 36,
+    information:
+      "Light, fresh and healthy grilled salmon Greek salad recipe. Crisp vegetables are tossed in a tangy lemon basil dressing and topped with flaky salmon.",
+    backgroundColor: "#FFEEDE",
+    textColor: "#F4AD69",
   },
   {
-    name: "",
+    name: "Asian Cucumber Salad",
     price: 35,
-    information: "",
-    backgroundColor: "#EAFFE2",
+    information:
+      "Asian Cucumber Salad Recipe made with crunchy cucumber, onion, rice wine vinegar, and a few secret ingredients!",
+    backgroundColor: "#F3FFDD",
+    textColor: "#9BC604",
   },
 ];
+
+export default meals;
